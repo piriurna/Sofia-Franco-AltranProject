238d8e800bdf01b69040e8017da662ce167b594b
Franco Zalamena <francozalamena@gmail.com>
2020-02-12 09:36:08 +0000

    idk

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,6 +2,9 @@ package application;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("");
+		String a = "ola";
+		String b = "ola";
+		a="alo";
+		System.out.println(a + " " + b);
 	}
 }
---
+++ src/application/controllers/colaborador/InsertColaborador.java
@@ -0,0 +1,5 @@
+package application.controllers.colaborador;
+
+public class InsertColaborador {
+	
+}
