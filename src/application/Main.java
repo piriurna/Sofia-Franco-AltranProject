414d2d95211f3a0235ceeb7e3c1afa80dbcf91fa
Franco Zalamena <francozalamena@gmail.com>
2020-02-12 11:08:56 +0000

    Remove method in DAO

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,5 +2,6 @@ package application;
 
 public class Main {
 	public static void main(String[] args) {
+		
 	}
 }
--- src/application/models/colaborador/ColaboradorDAO.java
+++ src/application/models/colaborador/ColaboradorDAO.java
@@ -13,10 +13,10 @@ public class ColaboradorDAO {
 
 	public static ArrayList<Colaborador> getColaboradores() {
 		ArrayList<Colaborador> colaboradores = new ArrayList<>();
-		Connection conn = DBConnector.getConnection("","","");
+		Connection conn = DBConnector.getConnection("", "", "");
 		String sql = "  ";
-		try(Statement stat = conn.createStatement(); ResultSet rs = stat.executeQuery(sql)){
-			while(rs.next()) {
+		try (Statement stat = conn.createStatement(); ResultSet rs = stat.executeQuery(sql)) {
+			while (rs.next()) {
 				String id = rs.getString(1);
 				String name = rs.getString(2);
 				TipoId tipoId = TipoId.valueOf(rs.getInt(3));
@@ -26,9 +26,33 @@ public class ColaboradorDAO {
 				String email = rs.getString(7);
 				String imagePath = rs.getString(8);
 				String cvPath = rs.getString(9);
-				colaboradores.add(new Colaborador(id, name, tipoId, nId, address,
-						cellphone, email, imagePath, cvPath));
+				colaboradores.add(new Colaborador(id, name, tipoId, nId, address, cellphone, email, imagePath, cvPath));
 			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return colaboradores;
+	}
+
+	public static void insertColaborador(Colaborador colaborador) {
+		Connection conn = DBConnector.getConnection("","","");
+		String sql = "";
+		try(PreparedStatement stat = conn.prepareStatement(sql)){
+			stat.setString(1, colaborador.getId());
+			stat.setString(2, colaborador.getName());
+			stat.setString(3, String.valueOf(colaborador.getTipoId()));
+			stat.setString(4, colaborador.getnId());
+			stat.setString(5, colaborador.getAddress());
+			stat.setString(6, colaborador.getCellphone());
+			stat.setString(7, colaborador.getEmail());
+			stat.setString(8, colaborador.getImagePath());
+			stat.setString(9, colaborador.getCvPath());
 		}catch(SQLException e) {
 			e.printStackTrace();
 		}finally {
@@ -38,21 +62,21 @@ public class ColaboradorDAO {
 				e.printStackTrace();
 			}
 		}
-		return colaboradores;
 	}
-	
-	public static Colaborador insertColaborador(Colaborador addColaborador) {
-		Colaborador colaborador = null;
-		Connection conn = DBConnector.getConnection("","","");
-		String sql = "  ";
-		try(PreparedStatement stat = conn.prepareStatement(sql)){
-			stat.setString(1, addColaborador.getId());
-			stat.setString(2, addColaborador.getName());
-			stat.setString(3, String.valueOf(addColaborador.getTipoId()));
-			stat.setString(4, addColaborador.getId());
-		}catch(SQLException e) {
+
+	public static void removeColaborador(Colaborador colaborador) {
+		Connection conn = DBConnector.getConnection("", "", "");
+		String sql = "";
+		try (PreparedStatement stat = conn.prepareStatement(sql)) {
+			stat.setString(1, colaborador.getId());
+		} catch (SQLException e) {
 			e.printStackTrace();
+		} finally {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
-		return null;
 	}
 }
