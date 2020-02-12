08949e031a1faeeca1f5d2f4f8299835b62e808f
FARIA CAPELO Sofia <sfariacapelo@europe.corp.altran.com>
2020-02-12 11:11:01 +0000

    delete

---
+++ src/application/controllers/colaborador/DeleteColaboradorController.java
@@ -0,0 +1,17 @@
+package application.controllers.colaborador;
+
+import application.models.colaborador.Colaborador;
+import application.models.colaborador.ColaboradorDAO;
+
+public class DeleteColaboradorController {
+	
+	public void deleteColaborador(Colaborador colaborador) {
+		
+			
+			System.out.println("Deleted");
+			ColaboradorDAO.deleteColaborador(colaborador);
+			
+			return;
+		}
+}
+			
