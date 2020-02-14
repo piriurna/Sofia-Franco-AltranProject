589189175f0e2f0e83ec450a38d5a30daca5116f
FARIA CAPELO Sofia <sfariacapelo@europe.corp.altran.com>
2020-02-14 13:45:23 +0000

    procedure

--- src/application/Main2.java
+++ src/application/Main2.java
@@ -2,9 +2,9 @@ package application;
 
 import application.models.user.UserDAO;
 
-public class Main {
+public class Main2 {
 	
-	public static void main(String args[]){ 
+	public static void main2(String args[]){ 
 	try{  
 		System.out.println(UserDAO.getAllUsers());
 		}catch(Exception e){ 
--- src/application/Start.java
+++ src/application/Start.java
@@ -1,6 +1,8 @@
 package application;
 
-import java.beans.Statement;
+import java.sql.Statement;
+import java.sql.Types;
+import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -10,19 +12,50 @@ import java.sql.SQLException;
 
 public class Start {
 
-	public static void mais(String[] args) {
+	public static void main(String[] args) {
 		
-		try (Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "system", "Nicolau061194@")) {
+		try (Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "system", "Pa$$w0rd")) {
 			
 			if(conn != null) { 
+			//Statement stmt=(Statement) conn.createStatement();
 				
-				Statement stmt=(Statement) conn.createStatement();
 				
-				String d="select * from emp where empid=' "+empid+"'";
+				String d="{call Login(?,?,?)}";
 				
-				ResultSet rd=((java.sql.Statement) stmt).executeQuery(d);
 				
-				System.out.println(""+d);
+				//Callable tipico para os statements
+				
+				CallableStatement stmt =conn.prepareCall(d);
+				
+				//parametro in - set
+				stmt.setString(1, "JSilva");
+				stmt.setString(2, "Pa$$w0rd");
+				
+				stmt.registerOutParameter(3,Types.VARCHAR);
+				
+				stmt.executeUpdate();
+				
+				
+				//guarda variavel 3
+				String SessionID=stmt.getString(3);
+				
+				System.out.println(SessionID);
+				
+				
+				//ResultSet rs=((java.sql.Statement) stmt).executeQuery(d);
+				
+//				int ID;
+//				String Nome;
+//				String NumcartaoCidadao;
+//				
+//				
+//				while (rs.next())
+//				{
+//					ID=rs.getInt("ID");
+//					Nome=rs.getString("Nome");
+//					NumcartaoCidadao=rs.getString("NumCartaoCidadao");
+//					System.out.println(ID+"\t"+Nome+"\t"+NumcartaoCidadao);
+//				}
 			}
 			else
 			{
